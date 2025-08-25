87fb95bfc4def4ed5ed9b430a85208d54fb91a7b
RhapsodyInGeek <44485952+RhapsodyInGeek@users.noreply.github.com>
2025-08-25 16:31:07 -0400

    Importer Finished

--- io_scene_swg/import_skt.py
+++ io_scene_swg/import_skt.py
@@ -1,8 +1,19 @@
-import bpy, base64
+# SKT Import / Export
+#
+# Authors:
+# Tim "RhapsodyInGeek" Maccabe (https://github.com/RhapsodyInGeek)
+# Vera "sinewavey" Lux (https://github.com/sinewavey)
+# Nick "NoStyleGuy" Rafalski (https://github.com/nostyleguy)
+# (c) 2025
+#
+# SWG's skeletons use joints instead of bones, which are just positional pivot points.
+# Blender bones have a head and a tail, so any bones without children to connect to 
+# require their tail positions to be guessed. This can lead to some strange looking 
+# bone orientations and may require manual post work to clean up.
+
+import bpy
 from . import swg_types
-from . import support
 from bpy.props import *
-from bpy_extras.io_utils import unpack_list, unpack_face_list
 from mathutils import Vector, Quaternion, Matrix
 import math
 
@@ -10,34 +21,25 @@ def swg_quat_to_blender_quat(r):
     q = Quaternion((-r[0], -r[1], r[2], r[3]))
     return q
 
-def rotate_point(point: Vector, pivot: Vector, rotation: Quaternion):
-    point -= pivot
-    point.rotate(rotation)
-    point += pivot
-    return point
-
 def import_skt(context, filepath):
-    #s = context.preferences.addons[__package__].preferences.swg_root
     skt = swg_types.SktFile(filepath)
     skt.load()
-
-    # Create armature
+    
     arm_name = filepath.split('\\')[-1].split('.')[0]
     arm = bpy.data.armatures.new(arm_name)
     arm_obj = bpy.data.objects.new(arm_name, arm)
     arm_obj.data.display_type = 'STICK'
     arm_obj.data.show_names = True
     arm_obj.show_in_front = True
-    #arm_obj.show_names = True
-
+    
     context.scene.collection.objects.link(arm_obj)
     arm_obj.select_set(True)
     context.view_layer.objects.active = arm_obj
-    bpy.ops.object.mode_set(mode = 'EDIT')
-
+    bpy.ops.object.mode_set(mode='EDIT')
+    
     bones = []
-
-    # Create bones
+    
+    # Create bones with initial positions
     for i in range(skt.joint_count):
         bone = arm.edit_bones.new(skt.joint_names[i])
         bones.append(bone)
@@ -45,49 +47,65 @@ def import_skt(context, filepath):
         bone.use_inherit_rotation = True
         t = skt.joint_translations[i]
         bone.head = Vector((-t[0], t[1], t[2]))
+        bone.tail = bone.head + Vector((0.0, 0.0, 0.1))
+        
+    def process_bone_hierarchy(bone_index, parent_transform=Matrix.Identity(4)):
+        if bone_index < 0 or bone_index >= skt.joint_count:
+            return
+            
+        bone = bones[bone_index]
 
-    # Parent bones and translate
-    for i in range(skt.joint_count):
-        bone = bones[i]
-        parent_id = skt.joint_parents[i]
-        if parent_id > -1:
+        rpre = swg_quat_to_blender_quat(skt.joint_pre_rotations[bone_index])
+        rbind = swg_quat_to_blender_quat(skt.joint_bind_rotations[bone_index])
+        rpost = swg_quat_to_blender_quat(skt.joint_post_rotations[bone_index])
+        
+        t = skt.joint_translations[bone_index]
+        local_translation = Vector((-t[0], t[1], t[2]))
+        
+        # Create transformation matrix
+        rotation_matrix = (rpost @ rbind @ rpre).to_matrix().to_4x4()
+        translation_matrix = Matrix.Translation(local_translation)
+        local_transform = translation_matrix @ rotation_matrix
+        
+        # Combine with parent transform
+        world_transform = parent_transform @ local_transform
+        
+        bone.head = world_transform.translation
+    
+        parent_id = skt.joint_parents[bone_index]
+        if parent_id >= 0:
             bone.parent = bones[parent_id]
-            bone.head += bone.parent.head
-
-    # Connect tails to children
+        
+        for i in range(skt.joint_count):
+            if skt.joint_parents[i] == bone_index:
+                process_bone_hierarchy(i, world_transform)
+    
+    for i in range(skt.joint_count):
+        if skt.joint_parents[i] < 0:
+            process_bone_hierarchy(i)
+    
+    # Set bone tails
     for i in range(skt.joint_count):
         bone = bones[i]
-        rpre = swg_quat_to_blender_quat(skt.joint_pre_rotations[i])
-        rpst = swg_quat_to_blender_quat(skt.joint_post_rotations[i])
-        bpro = swg_quat_to_blender_quat(skt.joint_bind_rotations[i])
-        r = rpst @ bpro @ rpre
         children = bone.children
-        for c in bone.children_recursive:
-            c.head = rotate_point(c.head, bone.head, rpre)
-            c.head = rotate_point(c.head, bone.head, bpro)
-            c.head = rotate_point(c.head, bone.head, rpst)
         ct = len(children)
+        
         if ct > 0:
             tail_pos = Vector((0.0, 0.0, 0.0))
-            for c in children:
-                tail_pos += c.head
+            for child in children:
+                tail_pos += child.head
             tail_pos /= ct
             bone.tail = tail_pos
             if ct == 1:
-                c.use_connect = True
+                children[0].use_connect = True
         elif bone.parent:
-            bone.tail = bone.head + (bone.parent.vector * len(bone.head - bone.parent.head) * 0.25)
-            bone.tail = rotate_point(bone.tail, bone.head, rpre)
-            bone.tail = rotate_point(bone.tail, bone.head, bpro)
-            bone.tail = rotate_point(bone.tail, bone.head, rpst)
+            v = bone.head - bone.parent.head
+            d = v / len(v)
+            bone.tail = bone.head + d * len(v) * 0.5
         else:
-            bone.tail = bone.head + Vector((0.0, 0.1, 0.0))
-            bone.tail = rotate_point(bone.tail, bone.head, rpre)
-            bone.tail = rotate_point(bone.tail, bone.head, bpro)
-            bone.tail = rotate_point(bone.tail, bone.head, rpst)
-    
-    # Apply rotations
-    bpy.ops.object.mode_set(mode = 'OBJECT')
+            bone.tail = bone.head + Vector((0.0, 0.0, 0.05))
+
+    bpy.ops.object.mode_set(mode='OBJECT')
     context.view_layer.objects.active = arm_obj
     arm_obj.rotation_euler = (math.pi * 0.5, 0.0, 0.0)
-    bpy.ops.object.transform_apply()
+    bpy.ops.object.transform_apply(rotation=True)
